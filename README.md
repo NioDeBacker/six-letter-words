d1941a6666c7ec0e725e5c53f0aeeabed5eacd5e
NioDeBacker <nio.debacker@student.hogent.be>
2023-04-02 17:39:43 +0200

    updates

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# six-letter-words
-Coding challenge for Kenze
+# 6 letter words
+
+Coding challenge for Kenze.
+
+To run the code, copy the repository and execute following commands
+
+```bash
+cd sixwords
+dotnet run
+```
+
+## Notes on solution
+
+- Not sure if relative path to get file is Windows proof (only tested on Mac)
+- The problem could be seen as a directional graph where each shorter part points to a larger string it e.g "a" {"ab", "ake"}. Possibly this could shorten the runtime if we build this graph while reading the lines. I decided not to pursue this option because of the time constraints.
+
+## Original challenge description
+
+There's a file in the root of the repository, input.txt, that contains words of varying lengths (1 to 6 characters).
+
+Your objective is to show all combinations of those words that together form a word of 6 characters. That combination must also be present in input.txt
+E.g.:
+
+```bash
+foobar  
+fo  
+obar  
+```
+
+should result in the ouput:  
+
+```bash
+fo+obar=foobar
+```
+
+You can start by only supporting combinations of two words and improve the algorithm at the end of the exercise to support any combinations.
+
+Treat this exercise as if you were writing production code; think unit tests, SOLID, clean code and avoid primitive obsession. Be mindful of changing requirements like a different maximum combination length, or a different source of the input data.
+
+The solution must be stored in a git repo. After the repo is cloned, the application should be able to run with one command / script.
+
+Don't spend too much time on this.
--- sixwords/Program.cs
+++ sixwords/Program.cs
@@ -6,8 +6,10 @@ string fileName = Path.Combine(Directory.GetCurrentDirectory(), "input.txt");
 ISet<string> maxLetterWords = new HashSet<string>();
 ISet<string> availableStrings = new HashSet<string>();
 
+// TODO: make this dynamic
 int maxLength = 6;
 
+// read words line by line
 using (StreamReader reader = new StreamReader(fileName)) {
     string word;
     while ((word = reader.ReadLine()) != null) {
@@ -19,24 +21,32 @@ using (StreamReader reader = new StreamReader(fileName)) {
 
     }
 }
+
+// error handling
+if (maxLetterWords.Count == 0 || availableStrings.Count == 0) {
+    throw new InvalidDataException($"No {maxLength} letter words or no parts smaller than {maxLength} found");
+}
+
+// for each 6 letter word we try to find their combinations
 foreach (string maxLetterWord in maxLetterWords) {
     BuildWord(maxLetterWord, "", new List<string>(), availableStrings);
 }
 
+// recursive function
 void BuildWord(string targetWord, string currentWord, IList<string> usedStrings, ISet<string> remainingStrings) {
+    // edge case
     if (currentWord.Length == maxLength) {
         Console.WriteLine($"{string.Join('+', usedStrings)} = {targetWord}");
     } else if (targetWord.Length > currentWord.Length) {
         int remainingLength = targetWord.Length - currentWord.Length;
+        // try building on the current word
         foreach (string part in remainingStrings) {
             if ( ( (part.Length <= remainingLength) && targetWord.Contains(currentWord+part) && !usedStrings.Contains(part) )) {
-                //Console.WriteLine(currentWord);
-                // Console.WriteLine(remainingLength);
-                // Console.WriteLine(string.Join("+",usedStrings));
                 List<string> newUsedStrings = new List<string>(usedStrings);
                 HashSet<string> newRemainingStrings = new HashSet<string>(remainingStrings);
                 newRemainingStrings.Remove(part);
                 newUsedStrings.Add(part);
+                // recursive step
                 BuildWord(targetWord, currentWord + part, newUsedStrings , newRemainingStrings);
             }
         }
